1249dc5b76c2d9f33633ab7960a9e281b0a68f97
Yadu Nand Babuji <yadunand@miralac1.fst.alcf.anl.gov>
2014-10-25 21:33:54 +0000

    Results

---
+++ aggregate_results.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare -a strings=("Sendrecv no delay" "Sendrecv wt delay" "Isend-recv no delay" "Isend-recv wt delay" "Isend-Irecv no delay" "Isend-Irecv wt delay" "12 at a time no delay" "12 at a time wt delay")
+
+for i in "TEST_2" "TEST_4" "TEST_8" "TEST_16"
+do
+    cd $i
+    echo "$i"
+    for search in "${strings[@]}"
+    do
+	#grep "$search" *error | awk '{print $(NF-1)}'
+	echo -n "$search in $i :  "
+	grep "$search" *error | awk '{ total += $(NF-1) } END { print total/NR }'
+    done
+    cd ..
+done
+
+
