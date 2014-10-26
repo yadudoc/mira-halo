5048b94de240ac0a5a08f17bf9fee055ecb1f7f5
Yadu Nand B <yadudoc1729@gmail.com>
2014-10-26 05:32:06 +0000

    Committing all

--- TEST_MAPPING_512/cobalt_script.sh
+++ TEST_MAPPING_512/cobalt_script.sh
@@ -4,9 +4,4 @@
 #COBALT -A ExM
 #COBALT -q prod-short
 
-MPI_RANKS=512
-RANKS_PER_NODE=1
-
-runjob --np $MPI_RANKS -p $RANKS_PER_NODE --block $COBALT_PARTNAME --cwd --mapping /home/yadunand/mira-halo/TEST_MAPPING_512/regular_map.txt : mmps 
-
-runjob --np $MPI_RANKS -p $RANKS_PER_NODE --block $COBALT_PARTNAME --cwd --mapping /home/yadunand/mira-halo/TEST_MAPPING_512/odd_map.txt : mmps 
+runjob 
