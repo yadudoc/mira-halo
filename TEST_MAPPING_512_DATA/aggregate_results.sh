a27e5b1f60193ab60e7927bf709d68725790898d
Yadu Nand B <yadudoc1729@gmail.com>
2014-10-26 17:07:38 +0000

    Adding results from 512 node/ mapping / data sizes

--- TEST_MAPPING_512_DATA/aggregate_results.sh
+++ TEST_MAPPING_512_DATA/aggregate_results.sh
@@ -2,17 +2,17 @@
 
 declare -a strings=("Sendrecv no delay" "Sendrecv wt delay" "Isend-recv no delay" "Isend-recv wt delay" "Isend-Irecv no delay" "Isend-Irecv wt delay" "12 at a time no delay" "12 at a time wt delay")
 
-for i in ("regular_4B.error" "regular_1024.error" "regular_10240.error" "odd_4.error" "odd_1024.error" "odd_10240.error")
+declare -a files=("regular_4B.error" "odd_4.error" "regular_1024.error"  "odd_1024.error" "regular_10240.error" "odd_10240.error")
+
+for i in ${files[@]}
 do
-    cd $i
     echo "$i"
     for search in "${strings[@]}"
     do
 	#grep "$search" *error | awk '{print $(NF-1)}'
 	echo -n "$search in $i :  "
-	grep "$search" *error | awk '{ total += $(NF-1) } END { print total/NR }'
-    done
-    cd ..
+	grep "$search" $i | awk '{ total += $(NF-1) } END { print total/NR }'
+    done 
 done
 
 
