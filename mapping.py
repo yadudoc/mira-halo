2ecce158ff7570659d9fd5b40b94b025419b642e
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-10-27 15:19:29 -0500

    More validation of input data

--- mapping.py
+++ mapping.py
@@ -5,11 +5,17 @@ import sys
 TORUS_DIMS = 5
 
 def load_mapping_file(mapping_file):
+  """
+  Load and do some validation of mapping
+  """
   print >> sys.stderr, "Loading mapping file: %s" % mapping_file
 
   failed = False
 
   rank2net = {}
+  mapped_coords = set()
+
+  max_coords = [0] * (TORUS_DIMS + 1)
 
   rank = 0
   with open(mapping_file, "r") as mf:
@@ -24,6 +30,13 @@ def load_mapping_file(mapping_file):
       for i in range(len(net_coords)):
         try:
           net_coords[i] = int(net_coords[i])
+          if net_coords[i] < 0:
+            print >> sys.stderr, "Invalid coordinate: %d" % net_coords[i]
+            bad_coord = True
+            failed = True
+            break
+          
+          max_coords[i] = max(max_coords[i], net_coords[i])
         except ValueError, e:
           print >> sys.stderr, "Invalid coordinates: %s" % str(net_coords)
           bad_coord = True
@@ -33,10 +46,28 @@ def load_mapping_file(mapping_file):
       if bad_coord:
         continue
       
+      if tuple(net_coords) in mapped_coords:
+        print >> sys.stderr, "Duplicate coordinates: %s" % str(net_coords)
+        failed = True
+        continue
+      mapped_coords.add(tuple(net_coords))
+
       rank2net[rank] = net_coords
       rank += 1
+
+  if not failed:
+    exp_ranks = 1
+    for max_coord in max_coords:
+      exp_ranks *= (max_coord + 1)
+
+    # Check if contiguous
+    if rank != exp_ranks:
+      print >> sys.stderr, "Gaps in mapping: product of dim sizes was %d but only %d mapping lines provided" % (exp_ranks, rank)
+      failed = True
+
   if failed:
-    raise ValueError("Mapping file load fail b/c of previous errors")
+      print >> sys.stderr, ("Mapping file load fail b/c of previous errors")
+      sys.exit(1)
 
   return rank2net
 
@@ -59,6 +90,11 @@ except ValueError, e:
 print >> sys.stderr, "nranks: %d" % nranks
 
 rank2net = load_mapping_file(sys.argv[2])
+
+if len(rank2net) != nranks:
+  print >> sys.stderr, ("Number of mapped ranks from mapping file %d " + \
+                        "doesn't match nranks %d") % (len(rank2net), nranks)
+
 print >> sys.stderr, "Rank2Net: %s" % str(rank2net)
 
 logical_dims = sys.argv[3].split(",")
