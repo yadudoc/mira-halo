ff9c2f8783b1e5d10342cc6f7e46641987a7ed7c
Yadu Nand Babuji <yadudoc1729@gmail.com>
2014-12-06 16:16:46 -0600

    Adding the boxed mapping file and generator for 5D case

--- TEST_RPN_16_NODE_512_5D/boxing.py
+++ TEST_RPN_16_NODE_512_5D/boxing.py
@@ -7,58 +7,47 @@ pp = pprint.PrettyPrinter(indent=4,depth=6)
 
 A_dim, B_dim, C_dim, D_dim, E_dim, T_dim = [4,4,4,4,2,16]
 
-
-def box_mapping():    
-    APP_DIMS  =(4,4,4,4,4)
-    PHYS_DIMS =(4,4,4,4,2,16)
-    APP_SLOTS = reduce( lambda x,y : x*y, APP_DIMS)
-    PHYS_SLOTS= reduce( lambda x,y : x*y, PHYS_DIMS)
-
-    if APP_SLOTS != PHYS_SLOTS :
-        print "ERROR! Total app slots do not match physical slots"
-        exit(1)
-        
-    print "Application dimensions : ", APP_DIMS
-    print "Physical dimensions    : ", PHYS_DIMS
-        
-    OUTER_DIM = []
-    for dim in PHYS_DIMS:
-        INNER_DIM = [ [x] for x in range(0,dim) ]
-        if not OUTER_DIM :
-            OUTER_DIM = INNER_DIM
-        else:
-            T = [ a+b for a in OUTER_DIM for b in INNER_DIM ]
-            OUTER_DIM = T
-
 def boxing_mapping(ofile):
     fd = open(ofile, 'w');
-    x = np.array(xrange(0, 8192)) # 4x4x4x4                                                                                                                                                   
-    print x.shape
+    x = np.array(xrange(0, 8192)) # 4x4x4x4
+    #print x.shape
     #origin = np.reshape(x, (4,4,4,4,2,16), order='C' )
-    origin = np.reshape(x, (32, 32, 8), order='C')
+    origin = np.reshape(x, (8,8,8,8,2), order='C')
 
     '''
-    App dimensions are 32x32x8
+    App dimensions are 8x8x8x8x2
     Phy dimensions are 4x4x4x4x2x16
     '''
-    for A in range(0, 32, 4):            # INCR 4
-        for B in range(0, 2):        # INCR 2
-            for C in range(0, 2):     # INCR 2
-                #print origin[A,B,C],  " :  ", A, B, C
-                a = A%16 # E = A/16 
-                print a/4, (B/2)%4, int(C/2), int(B/8), int(A/16), ( (a%4 * 4) + ( B%2 * 2) + (C%2) )
-    #pp.pprint(y)
-    #pp.pprint(origin[0,0:4,0:4,0:4,0:2,0:8])
-    
-    '''
-    for index in range(0,8192):
-        location = np.where(origin==index)
-        fd.write('{0} {1} {2} {3} {4} {5}\n'.format(location[0][0], location[1][0], location[2][0], location[3][0], location[4][0], location[5][0]))
-        print location[0][0], location[1][0], location[2][0], location[3][0], location[4][0], location[5][0]
-    fd.close
-    '''
+    target = {}
+    for E in range(0,2):
+        for A in range(0, 8, 2):            # INCR 4
+            for B in range(0, 8, 2):         # INCR 2
+                for C in range(0, 8, 2):        # INCR 2
+                    for D in range(0, 8, 2):
 
-boxing_mapping("foo");
+                        a = A/2
+                        b = B/2
+                        c = C/2
+                        d = D/2
+                        e = E
+                        #print a, b, c, d, E
+                        slice = origin[A:A+2, B:B+2, C:C+2, D:D+2, E]
+                        #print "Slice for {0} {1} {2} {3} {4}:  {5}".format(A,B,C,D,E,slice)
+                        items = np.reshape(slice, (16), order='C')
+                        for core,item in enumerate(items):
+                            target[item] = "{0} {1} {2} {3} {4} {5}".format(a,b,c,d,e,core)
+                            #print "target[{0}] = {1}".format(item, target[item])
+
+    for index in range(0,8192):
+        fd.write('{0}\n'.format(target[index]))
+        #fd.write('{0} {1} {2} {3} {4} {5}\n'.format(location[0][0], location[1][0], location[2][0], location[3][0], location[4][0], location[5][0]))
+        #print location[0][0], location[1][0], location[2][0], location[3][0], location[4][0], location[5][0]
+    fd.close
+
+    return
+
+
+boxing_mapping("box.txt");
 
 #regular_mapping("regular_mapping.txt");
 #line_mapping("linear_mapping.txt");
